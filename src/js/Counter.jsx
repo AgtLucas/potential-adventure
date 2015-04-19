073a9be4400c36477e78ba7e11910f3122ccce5b
Lucas <lucas@agtlucas.com>
2015-04-19 00:12:26 -0300

    Updates

--- src/js/Counter.jsx
+++ src/js/Counter.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 class Counter extends React.component {
-  state = { count: 0 };
+  getInitialState: function() {
+    return {
+      this.state = { count: 0 };
+    };
+  },
 
   componentDidMount() {
     setInterval( () => this.tick(), 1000);
-  }
+  },
 
   tick() {
     var count = this.state.count + 1;
     this.setState({count});
-  }
+  },
 
   render() {
     return <h1>Count: {this.state.count}</h1>;

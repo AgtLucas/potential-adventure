5865ca9e8fcdf1e95a4fc590108d27733bf1c1cb
Lucas <lucas@agtlucas.com>
2015-04-18 22:48:58 -0300

    Update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx$/, loader: 'jsx-loader' },
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'}
     ]
   }
 }

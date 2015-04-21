66d5b4e9a4116dc133950d967058a3535c3afd9d
Lucas <lucas@agtlucas.com>
2015-04-21 19:50:23 -0300

    Update webpack local

--- webpack.local.config.js
+++ webpack.local.config.js
@@ -17,4 +17,19 @@ module.exports = {
     filename: 'app.js',
     publicPath: 'http://localhost:9090/build'
   },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ['react-hot', 'babel-loader'] }
+    ]
+  },
+
+  resolve: {
+    extensions: ['', '.js', 'jsx']
+  }
 }

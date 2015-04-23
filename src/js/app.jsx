c732759b2d21aff6010273d1daa7aff6e1beb68a
Lucas <lucas@agtlucas.com>
2015-04-23 08:42:11 -0300

    Update app.jsx

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -1,3 +1,16 @@
 // ES6!!!
 import 'babel-core/polyfill';
 import React from 'react';
+import {Body} from './foo';
+
+class _MainSection {
+  render() {
+    return (
+      <div>
+        <Body />
+      </div>
+    );
+  }
+}
+
+export const MainSection = React.createClass(_MainSection.prototype);
